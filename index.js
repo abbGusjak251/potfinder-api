7f7e0199c47dc04cee78fa851e4a8d3d7f294562
Gustav Jakobsson <gustav.jakobsson@abbgymnasiet.se>
2022-11-15 10:13:06 +0100

    Added connection to postgres database

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes.js');
+const cors = require('cors');
 
 require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+'/public/index.html');
+});
 
 app.use('/api', routes);
 
@@ -12,4 +18,4 @@ app.get('/api', (req, res) => {
     res.send("Welcome to Potfinder API");
 });
 
-app.listen(process.env.PORT, () => console.log("Port is " + process.env.PORT));
+app.listen(process.env.PORT || 3000, () => console.log("Port is " + process.env.PORT));
--- routes.js
+++ routes.js
@@ -1,12 +1,57 @@
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
+const { client } = require('./db.js');
 
-router.get('/data', (req, res) => {
-    res.send("Get");
+const connectDB = async() => {
+    await client.connect();
+    console.log("Connected");
+};
+
+connectDB();
+
+router.get('/data', async(req, res) => {
+    // Authorize request
+    if (!(req.body.secret_key === process.env.SECRET_KEY)) return res.status(401).json({message: 'Unauthorized request'});
+    try {
+        // Fetch all rows from table Measurements
+        const data = await client.query('SELECT * FROM Measurements');
+        return res.json(data).send();
+    } catch(err) {
+        // Log and send error to client
+        console.error(err);
+        return res.status(400).json({message: err.message});
+    }
 });
 
-router.post('/data', (req, res) => {
-    res.send("Post");
+router.get('/data/:id', async(req, res) => {
+    // Authorize request
+    if (!(req.body.secret_key === process.env.SECRET_KEY)) return res.status(401).json({message: 'Unauthorized request'});
+    try {
+        // Fetch row from table Measurements where id is equal to id parameter
+        const id = req.params.id;
+        const data = await client.query(`SELECT * FROM Measurements WHERE id < $1 AND id > $2`, [id+1, id-1]);
+        return res.json(data).send();
+    } catch(err) {
+        // Log and send error to client
+        console.error(err);
+        return res.status(400).json({message: err.message});
+    }
+});
+
+router.post('/data', async (req, res) => {
+    if (!(req.body.secret_key === process.env.SECRET_KEY)) return res.status(401).json({message: 'Unauthorized request'});
+    try {
+        const { lon, lat } = req.body;
+        const timestamp = new Date();
+        const type = "a";
+        const id = Math.floor(Math.random()*10);
+        const data = [lon, lat, timestamp, type];
+        await client.query(`INSERT INTO Measurements VALUES(DEFAULT, $1, $2, $3, $4)`, data);
+        return res.status(200).send(data);
+    } catch(err) {
+        console.error(err);
+        return res.status(400).json({message: err.message});
+    }
 });
 
 router.delete('/data', (req, res) => {
